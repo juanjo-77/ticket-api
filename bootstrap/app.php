b5d3d51fa879a58db24187d91a8bdccd292fb5af
root <root@vmi3240640.contaboserver.net>
2026-05-11 22:39:13 +0200

    feat: docker configurado y migraciones listas

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,17 +6,14 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-    web: __DIR__.'/../routes/web.php',
-    api: __DIR__.'/../routes/api.php',
-    commands: __DIR__.'/../routes/console.php',
-    health: '/up',
-)
+        web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
+        commands: __DIR__.'/../routes/console.php',
+        health: '/up',
+    )
     ->withMiddleware(function (Middleware $middleware) {
-    $middleware->throttle([
-        'api' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-    ]);
-    $middleware->append(\App\Http\Middleware\DiscordRateLimitAlert::class);
-})
+        $middleware->append(\App\Http\Middleware\DiscordRateLimitAlert::class);
+    })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
